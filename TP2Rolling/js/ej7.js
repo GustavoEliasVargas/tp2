9b21ee0254b1bb493ad07a136205fb14d8ade62d
Vargas Elias Gustavo <gustavo.elias.v@gmail.com>
2023-11-06 00:45:26 -0300

    correccion de limites de ingreso

--- TP2Rolling/js/ej7.js
+++ TP2Rolling/js/ej7.js
@@ -2,7 +2,7 @@ document.write('<article>');
 document.write('<h2>Ejercicio 7</h2>');
 let cantidad = parseInt(prompt('ingrese la cantida de elemntos del 1 al 50'));
 console.log(cantidad);
-if (typeof(cantidad)==='number'){
+if (typeof(cantidad)==='number' && (cantidad>=1 && cantidad<=50)){
   for (let i = cantidad; i > 0; i--){
     console.log(i);
     for (let j=1; j<=i;j++){
---
+++ TP2Rolling/js/ej8.js
@@ -0,0 +1,4 @@
+document.write('<article>');
+document.write('<h2>Ejercicio 8</h2>');
+
+document.write('</article>');
